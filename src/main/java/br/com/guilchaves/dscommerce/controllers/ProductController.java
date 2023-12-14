6edafdd74b4f78595ff678efe71043370caee4ba
guilherme <chaves.guilherme@proton.me>
2023-12-14 16:33:54 -0300

    feat: delete product

--- src/main/java/br/com/guilchaves/dscommerce/controllers/ProductController.java
+++ src/main/java/br/com/guilchaves/dscommerce/controllers/ProductController.java
@@ -46,9 +46,15 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
