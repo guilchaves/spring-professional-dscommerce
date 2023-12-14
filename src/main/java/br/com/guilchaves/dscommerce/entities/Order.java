df6372aef7d8b03278280b30d0b1fa2a1c67f0e7
guilherme <chaves.guilherme@proton.me>
2023-12-14 14:20:10 -0300

    feat: add getters, setters, equals&hash, serial to Order

--- src/main/java/br/com/guilchaves/dscommerce/entities/Order.java
+++ src/main/java/br/com/guilchaves/dscommerce/entities/Order.java
@@ -2,11 +2,16 @@ package br.com.guilchaves.dscommerce.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
-public class Order {
+public class Order implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +24,52 @@ public class Order {
     public User client;
 
     public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
