c15c8f7c00f12bf6fb84add0f3ffc40f4e4ccfa6
guilherme <chaves.guilherme@proton.me>
2023-12-14 16:31:08 -0300

    feat: update product

--- src/main/java/br/com/guilchaves/dscommerce/services/ProductService.java
+++ src/main/java/br/com/guilchaves/dscommerce/services/ProductService.java
@@ -36,6 +36,14 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        Product entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
