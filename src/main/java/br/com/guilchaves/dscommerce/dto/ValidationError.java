541e94df1b96f34123c6a510be9e6ddab4e424ca
guilherme <chaves.guilherme@proton.me>
2023-12-15 18:14:40 -0300

    fix: stop duplicating errors on validation error msg

--- src/main/java/br/com/guilchaves/dscommerce/dto/ValidationError.java
+++ src/main/java/br/com/guilchaves/dscommerce/dto/ValidationError.java
@@ -17,6 +17,7 @@ public class ValidationError extends CustomError {
     }
 
     public void addErrors(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
         errors.add(new FieldMessage(fieldName, message));
     }
 }
