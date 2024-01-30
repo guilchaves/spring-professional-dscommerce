4bcac777daf95301ba5e6cea61a5bb557f7ba118
guilherme chaves <gchaves.guilherme@gmail.com>
2024-01-30 18:42:18 -0300

    refac: use rest assured to order controller it

--- src/test/java/br/com/guilchaves/dscommerce/controllers/OrderControllerRA.java
+++ src/test/java/br/com/guilchaves/dscommerce/controllers/OrderControllerRA.java
@@ -1,120 +1,119 @@
 package br.com.guilchaves.dscommerce.controllers;
 
 import br.com.guilchaves.dscommerce.tests.TokenUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
-import org.springframework.http.MediaType;
+public class OrderControllerRA {
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-public class OrderControllerIT {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private TokenUtil tokenUtil;
-
-    String adminUsername, adminPassword;
-    String clientUsername, clientPassword;
-    String adminToken, clientToken;
-    Long nonExistingId, existinId, notOwnedId;
-    Long clientId, adminId;
-
+    private String adminUsername, adminPassword;
+    private String clientUsername, clientPassword;
+    private String adminToken, clientToken, invalidToken;
+    private Long nonExistingId, existingId, forbiddenId;
 
     @BeforeEach
     void setUp() throws Exception {
+        baseURI = "http://localhost:8080";
+
         clientUsername = "maria@gmail.com";
         clientPassword = "123456";
-        clientId = 1L;
         adminUsername = "alex@gmail.com";
         adminPassword = "123456";
+
         nonExistingId = 0L;
-        existinId = 1L;
-        notOwnedId = 2L;
+        existingId = 1L;
+        forbiddenId = 2L;
 
-        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
-        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto";
     }
 
     @Test
-    public void findByIdShouldReturnOrderWhenIdExistsAndAdminLogged() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", existinId)
-                        .header("Authorization", "Bearer " + adminToken)
-                        .accept(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isOk());
-        result.andExpect(jsonPath("$.id").value(existinId));
+    public void findByIdShouldReturnOrderWhenIdExistsAdminLogger() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", existingId)
+                .then()
+                .statusCode(200)
+                .body("id", is(Integer.parseInt(String.valueOf(existingId))))
+                .body("client.id", is(1))
+                .body("client.name", is("Maria Brown"));
     }
 
     @Test
-    public void findByIdShouldReturnOrderWhenOrderSelfOwnedAndSelfClientLogged() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", existinId)
-                        .header("Authorization", "Bearer " + clientToken)
-                        .accept(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isOk());
-        result.andExpect(jsonPath("$.id").value(existinId));
-        result.andExpect(jsonPath("$.client.id").value(clientId));
-        result.andExpect(jsonPath("$.items").isArray());
+    public void findByIdShouldReturnOrderWhenOrderSelfOwnedAndSelfClientLogged() {
+        existingId = 3L;
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", existingId)
+                .then()
+                .statusCode(200)
+                .body("id", is(Integer.parseInt(String.valueOf(existingId))))
+                .body("client.id", is(1))
+                .body("client.name", equalTo("Maria Brown"))
+                .body("status", equalTo("WAITING_PAYMENT"))
+                .body("items[0].name", equalTo("The Lord of the Rings"));
     }
 
     @Test
-    public void findByIdShouldReturnForbiddenWhenOrderNotSelfOwnedAndClientLogged() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", notOwnedId)
-                        .header("Authorization", "Bearer " + clientToken)
-                        .accept(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isForbidden());
+    public void findByIdShouldReturnForbiddenWhenOrderNotSelfOwnedAndClientLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", forbiddenId)
+                .then()
+                .statusCode(403);
     }
 
     @Test
-    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndAdminLogged() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", nonExistingId)
-                        .header("Authorization", "Bearer " + adminToken)
-                        .accept(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isNotFound());
+    public void findByIdShouldReturnResourceNotFoundWhenIdDoesNotExistsAndAdminLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", nonExistingId)
+                .then()
+                .statusCode(404);
     }
 
     @Test
-    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndClientLogged() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", nonExistingId)
-                        .header("Authorization", "Bearer " + clientToken)
-                        .accept(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isNotFound());
+    public void findByIdShouldReturnResourceNotFoundWhenIdDoesNotExistsAndClientLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", nonExistingId)
+                .then()
+                .statusCode(404);
     }
 
-    @Test
-    public void findByIdShouldReturnUnauthorizedWhenLoggedOff() throws Exception {
-        ResultActions result =
-                mockMvc.perform(get("/orders/{id}", existinId)
-                        .header("Authorization", "Bearer ")
-                        .accept(MediaType.APPLICATION_JSON));
 
-        result.andExpect(status().isUnauthorized());
+    @Test
+    public void findByIdShouldReturnUnauthorizedWhenNotLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + invalidToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", existingId)
+                .then()
+                .statusCode(401);
     }
 }
