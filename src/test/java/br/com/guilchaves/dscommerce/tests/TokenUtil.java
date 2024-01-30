9dcf9e184cce8c59684f9b050088398b7f17c699
guilherme chaves <gchaves.guilherme@gmail.com>
2024-01-30 18:39:54 -0300

    refac: refac to use rest assured

--- src/test/java/br/com/guilchaves/dscommerce/tests/TokenUtil.java
+++ src/test/java/br/com/guilchaves/dscommerce/tests/TokenUtil.java
@@ -1,44 +1,29 @@
 package br.com.guilchaves.dscommerce.tests;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.stereotype.Component;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static io.restassured.RestAssured.given;
 
-@Component
 public class TokenUtil {
-    @Value("${security.client-id}")
-    private String clientId;
+    public static String obtainAccessToken(String username, String password) {
+        Response response = authRequest(username, password);
+        JsonPath jsonBody = response.jsonPath();
+        return jsonBody.getString("access_token");
 
-    @Value("${security.client-secret}")
-    private String clientSecret;
-
-    public String obtainAccessToken(MockMvc mockMvc, String username, String password) throws Exception {
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "password");
-        params.add("username", username);
-        params.add("password", password);
-
-        ResultActions result = mockMvc
-                .perform(post("/oauth2/token")
-                        .params(params)
-                        .with(httpBasic(clientId, clientSecret))
-                        .accept("application/json;charset=UTF-8"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"));
-
-        String resultString = result.andReturn().getResponse().getContentAsString();
-
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        return jsonParser.parseMap(resultString).get("access_token").toString();
     }
+
+    private static Response authRequest(String username, String password) {
+        return given()
+                .auth()
+                .preemptive()
+                .basic("myclientid", "myclientsecret")
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("grant_type", "password")
+                .formParam("username", username)
+                .formParam("password", password)
+                .when()
+                .post("/oauth2/token");
+    }
+
 }
